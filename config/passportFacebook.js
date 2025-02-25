e9d110e5a23c736df6f1212857b6968828c069ef
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-02-25 12:00:58 -0500

    Implementación de mejoras para comunicación con facebook OAuth

--- config/passportFacebook.js
+++ config/passportFacebook.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const User = require('@models/User');
+const { generateRefreshToken } = require('@utils/refreshToken');
+const { apiLogger } = require('@utils/logger');
 
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID, 
@@ -14,14 +16,20 @@ passport.use(new FacebookStrategy({
         if (!user) {
             user = await User.create({
                 username: profile.displayName,
-                email: profile.emails ? profile.emails[0].value : `${profile.id}@facebook.com`,
+                email: profile.emails ? profile.emails[0].value : null,
                 provider: 'facebook',
                 providerId: profile.id,
+                refreshToken: generateRefreshToken(profile.id), 
             });
+        } else {
+            // Si el usuario ya existe, actualizamos su Refresh Token
+            user.refreshToken = generateRefreshToken(user.id);
+            await user.save();
         }
 
         return done(null, user);
     } catch (error) {
+        apiLogger.error(`[passportFacebook]: ${error.message}`, { stack: error.stack });
         return done(error, null);
     }
 }));
