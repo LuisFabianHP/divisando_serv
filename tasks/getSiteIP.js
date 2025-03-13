9030f7ad801dadc9d55e2a1e18fb5b1845e0a0cb
LUIS FABIAN HP <ironluis.js@gmail.com>
2025-03-12 21:39:13 -0400

    Corrección a script para obtener IP, usnado nueva estrategia

--- tasks/getSiteIP.js
+++ tasks/getSiteIP.js
@@ -1,9 +1,17 @@
-const http = require('http');
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end(`Server IP: ${ip}\n`);
-}).listen(3000, () => {
-    console.log('Test server running on port 3000');
+app.get('/mi-ip', async (req, res) => {
+  try {
+    const respuesta = await axios.get('https://api.ipify.org?format=json');
+    res.send(`La IP pública del servidor es: ${respuesta.data.ip}`);
+  } catch (error) {
+    res.status(500).send('No se pudo obtener la IP pública.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
